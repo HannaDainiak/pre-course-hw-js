06f849f889808e0cf1053f690e567ddf0f3af43a
HannaDainiak <hannakam09@gmail.com>
2024-04-14 12:19:15 +0300

    test3-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    const germanyColors = colors.filter(color => color === 'черный' || color === 'красный' || color === 'желтый');
+    return germanyColors.join('-');
+}
+
+console.log(createColorString());
